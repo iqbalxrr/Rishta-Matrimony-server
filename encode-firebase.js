ec91ff94d482ee3fdb19f12a9a732fa327d344ec
iqbalxrr <iamekbal7500@gmail.com>
2025-07-19 01:26:27 +0600

    First commit

---
+++ encode-firebase.js
@@ -0,0 +1,8 @@
+// const fs = require('fs');
+
+// const json = fs.readFileSync('./rishta.json', 'utf-8');
+// const base64 = Buffer.from(json).toString('base64');
+
+// console.log('Base64 Encoded String:\n');
+// console.log(base64);
+
